2955b42cad230427a1af4c767b57e30db7018ff4
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-03-26 10:51:25 +0100

    Rebase from 'debian/rolling/image_proc'

--- src/track_marker.cpp
+++ src/track_marker.cpp
@@ -1,4 +1,4 @@
-// Copyright 2008, 2019 Willow Garage, Inc., Steve Macenski, Joshua Whitley
+// Copyright 2024 Open Navigation LLC
 // All rights reserved.
 //
 // Software License Agreement (BSD License 2.0)
@@ -30,28 +30,25 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
-#include <algorithm>
+#include <cstddef>
 #include <functional>
-#include <iterator>
+#include <memory>
 #include <string>
 #include <vector>
 
-#include "cv_bridge/cv_bridge.hpp"
-
-#include <image_proc/crop_non_zero.hpp>
+#include <cv_bridge/cv_bridge.hpp>
+#include <image_proc/track_marker.hpp>
 #include <image_proc/utils.hpp>
-
 #include <image_transport/image_transport.hpp>
-#include <opencv2/imgproc.hpp>
+#include <opencv2/core/quaternion.hpp>
+#include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/image_encodings.hpp>
-#include <sensor_msgs/msg/image.hpp>
 
 namespace image_proc
 {
 
-CropNonZeroNode::CropNonZeroNode(const rclcpp::NodeOptions & options)
-: Node("CropNonZeroNode", options)
+TrackMarkerNode::TrackMarkerNode(const rclcpp::NodeOptions & options)
+: rclcpp::Node("TrackMarkerNode", options)
 {
   // TransportHints does not actually declare the parameter
   this->declare_parameter<std::string>("image_transport", "raw");
@@ -59,83 +56,95 @@ CropNonZeroNode::CropNonZeroNode(const rclcpp::NodeOptions & options)
   // For compressed topics to remap appropriately, we need to pass a
   // fully expanded and remapped topic name to image_transport
   auto node_base = this->get_node_base_interface();
-  image_topic_ = node_base->resolve_topic_or_service_name("image_raw", false);
-  std::string pub_topic = node_base->resolve_topic_or_service_name("image", false);
+  image_topic_ = node_base->resolve_topic_or_service_name("image", false);
+
+  // Declare parameters before we setup any publishers or subscribers
+  marker_id_ = this->declare_parameter("marker_id", 0);
+  marker_size_ = this->declare_parameter("marker_size", 0.05);
+  // Default dictionary is cv::aruco::DICT_6X6_250
+  int dict_id = this->declare_parameter("dictionary", 10);
+
+  detector_params_ = cv::aruco::DetectorParameters::create();
+  dictionary_ = cv::aruco::getPredefinedDictionary(dict_id);
 
   // Setup lazy subscriber using publisher connection callback
   rclcpp::PublisherOptions pub_options;
   pub_options.event_callbacks.matched_callback =
     [this](rclcpp::MatchedInfo &)
     {
-      if (pub_.getNumSubscribers() == 0) {
-        sub_raw_.shutdown();
-      } else if (!sub_raw_) {
+      if (pub_->get_subscription_count() == 0) {
+        sub_camera_.shutdown();
+      } else if (!sub_camera_) {
         // Create subscriber with QoS matched to subscribed topic publisher
         auto qos_profile = getTopicQosProfile(this, image_topic_);
         image_transport::TransportHints hints(this);
-        sub_raw_ = image_transport::create_subscription(
+        sub_camera_ = image_transport::create_camera_subscription(
           this, image_topic_, std::bind(
-            &CropNonZeroNode::imageCb, this,
-            std::placeholders::_1), hints.getTransport(), qos_profile);
+            &TrackMarkerNode::imageCb,
+            this, std::placeholders::_1, std::placeholders::_2),
+          hints.getTransport(), qos_profile);
       }
     };
 
   // Allow overriding QoS settings (history, depth, reliability)
   pub_options.qos_overriding_options = rclcpp::QosOverridingOptions::with_default_policies();
 
-  // Create publisher with QoS matched to subscribed topic publisher
-  auto qos_profile = getTopicQosProfile(this, image_topic_);
-  pub_ = image_transport::create_publisher(this, pub_topic, qos_profile, pub_options);
+  // Create publisher
+  pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(
+    "tracked_pose", 10, pub_options);
 }
 
-void CropNonZeroNode::imageCb(const sensor_msgs::msg::Image::ConstSharedPtr & raw_msg)
+void TrackMarkerNode::imageCb(
+  const sensor_msgs::msg::Image::ConstSharedPtr & image_msg,
+  const sensor_msgs::msg::CameraInfo::ConstSharedPtr & info_msg)
 {
-  cv_bridge::CvImagePtr cv_ptr;
+  cv_bridge::CvImageConstPtr cv_ptr;
   try {
-    cv_ptr = cv_bridge::toCvCopy(raw_msg);
+    cv_ptr = cv_bridge::toCvShare(image_msg);
   } catch (cv_bridge::Exception & e) {
     RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
     return;
   }
 
-  // Check the number of channels
-  if (sensor_msgs::image_encodings::numChannels(raw_msg->encoding) != 1) {
-    RCLCPP_ERROR(
-      this->get_logger(), "Only grayscale image is acceptable, got [%s]",
-      raw_msg->encoding.c_str());
-    return;
+  std::vector<int> marker_ids;
+  std::vector<std::vector<cv::Point2f>> marker_corners;
+  cv::aruco::detectMarkers(cv_ptr->image, dictionary_, marker_corners, marker_ids);
+
+  for (size_t i = 0; i < marker_ids.size(); ++i) {
+    if (marker_ids[i] == marker_id_) {
+      // This is our desired marker
+      std::vector<std::vector<cv::Point2f>> corners;
+      corners.push_back(marker_corners[i]);
+
+      // Copy the matrices since they are const and OpenCV functions are not
+      auto k = info_msg->k;
+      auto d = info_msg->d;
+
+      // Get the camera info
+      cv::Mat intrinsics(3, 3, CV_64FC1, reinterpret_cast<void *>(k.data()));
+      cv::Mat dist_coeffs(info_msg->d.size(), 1, CV_64FC1, reinterpret_cast<void *>(d.data()));
+
+      // Estimate pose
+      std::vector<cv::Vec3d> rvecs, tvecs;
+      cv::aruco::estimatePoseSingleMarkers(
+        corners, marker_size_, intrinsics, dist_coeffs, rvecs, tvecs);
+
+      // Publish pose of marker
+      geometry_msgs::msg::PoseStamped pose;
+      pose.header = image_msg->header;
+      // Fill in pose
+      pose.pose.position.x = tvecs[0][0];
+      pose.pose.position.y = tvecs[0][1];
+      pose.pose.position.z = tvecs[0][2];
+      // Convert angle-axis to quaternion
+      cv::Quatd q = cv::Quatd::createFromRvec(rvecs[0]);
+      pose.pose.orientation.x = q.x;
+      pose.pose.orientation.y = q.y;
+      pose.pose.orientation.z = q.z;
+      pose.pose.orientation.w = q.w;
+      pub_->publish(pose);
+    }
   }
-  std::vector<std::vector<cv::Point>> cnt;
-  cv::Mat1b m(raw_msg->width, raw_msg->height);
-
-  if (raw_msg->encoding == sensor_msgs::image_encodings::TYPE_8UC1) {
-    m = cv_ptr->image;
-  } else {
-    double minVal, maxVal;
-    cv::minMaxIdx(cv_ptr->image, &minVal, &maxVal, 0, 0, cv_ptr->image != 0.);
-    double ra = maxVal - minVal;
-
-    cv_ptr->image.convertTo(m, CV_8U, 255. / ra, -minVal * 255. / ra);
-  }
-
-  cv::findContours(m, cnt, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-
-  // search the largest area
-  std::vector<std::vector<cv::Point>>::iterator it =
-    std::max_element(
-    cnt.begin(), cnt.end(), [](std::vector<cv::Point> a,
-    std::vector<cv::Point> b) {
-      return a.size() < b.size();
-    });
-
-  cv::Rect r = cv::boundingRect(cnt[std::distance(cnt.begin(), it)]);
-
-  cv_bridge::CvImage out_msg;
-  out_msg.header = raw_msg->header;
-  out_msg.encoding = raw_msg->encoding;
-  out_msg.image = cv_ptr->image(r);
-
-  pub_.publish(out_msg.toImageMsg());
 }
 
 }  // namespace image_proc
@@ -145,4 +154,4 @@ void CropNonZeroNode::imageCb(const sensor_msgs::msg::Image::ConstSharedPtr & ra
 // Register the component with class_loader.
 // This acts as a sort of entry point, allowing the component to be discoverable when its library
 // is being loaded into a running process.
-RCLCPP_COMPONENTS_REGISTER_NODE(image_proc::CropNonZeroNode)
+RCLCPP_COMPONENTS_REGISTER_NODE(image_proc::TrackMarkerNode)
