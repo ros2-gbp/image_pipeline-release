7d2b70ccfa06d62223bb4bf4455f124612e4ba7f
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-02-13 10:53:00 +0100

    Imported upstream version '5.0.8' of 'upstream'

--- image_proc/src/track_marker.cpp
+++ image_proc/src/track_marker.cpp
@@ -30,6 +30,8 @@
 // ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 
+#include <tf2/LinearMath/Quaternion.h>
+
 #include <cstddef>
 #include <functional>
 #include <memory>
@@ -43,7 +45,6 @@
 #include <rclcpp/qos.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <tf2/LinearMath/Quaternion.hpp>
 
 namespace image_proc
 {
