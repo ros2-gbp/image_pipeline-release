69768095f658cb08526f613e61d2ab6aa9109e9d
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-09-01 10:25:24 +0200

    Rebase from 'upstream'

--- stereo_image_proc/src/stereo_image_proc/point_cloud_node.cpp
+++ stereo_image_proc/src/stereo_image_proc/point_cloud_node.cpp
@@ -183,14 +183,14 @@ PointCloudNode::PointCloudNode(const rclcpp::NodeOptions & options)
         const auto sensor_data_qos = rclcpp::SensorDataQoS();
 
         // Support image transport for compression
-        image_transport::TransportHints hints(this);
+        image_transport::TransportHints hints{*this};
 
         // Allow overriding QoS settings (history, depth, reliability)
         auto sub_opts = rclcpp::SubscriptionOptions();
         sub_opts.qos_overriding_options = rclcpp::QosOverridingOptions::with_default_policies();
 
         sub_l_image_.subscribe(
-          this, left_topic, hints.getTransport(), sensor_data_qos, sub_opts);
+          *this, left_topic, hints.getTransport(), sensor_data_qos, sub_opts);
         sub_l_info_.subscribe(this, left_info_topic,
           sensor_data_qos, sub_opts);
         sub_r_info_.subscribe(this, right_topic,
